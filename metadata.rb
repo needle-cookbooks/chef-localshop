b75b673573bbab0a0ecb127dcc5197606b9e04a6
Matt Jones <matt.jones@needle.com>
2015-02-26 11:54:22 -0700

    Bumped version in metadata to reflect version uploaded

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,4 @@ description      "Installs/Configures localshop caching pypi server"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.3.0"
 
-depends 'application_python', '~> 16.0.0'
+depends 'application_python', '~> 16.2.0'
